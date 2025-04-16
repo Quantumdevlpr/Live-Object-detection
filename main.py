78cbf9c0f7f968a94ed7fe55c4c6b5081ced88d1
shivansh <mshivanshmaurya@gmail.com>
2025-04-16 11:31:46 +0530

    "importing libr"

---
+++ main.py
@@ -0,0 +1,7 @@
+# i want to create a live object detection app 
+import cv2
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
